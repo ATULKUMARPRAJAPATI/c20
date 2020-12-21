afdd20e559c0244bc8317ef7ed6dcf28b62f0323
ATULKUMARPRAJAPATI <75626829+ATULKUMARPRAJAPATI@users.noreply.github.com>
2020-12-21 20:44:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var fixedRect,movingRect;
+
+function setup() {
+  createCanvas(1200,1200);
+ fixedRect=createSprite(500,400,200,100);
+ movingRect=createSprite(400,500,100,100);
+
+}
+
+function draw() {
+  background(255,255,255);  
+
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+
+if(movingRect.x-fixedRect.x< movingRect.width/2+fixedRect.width/2 && fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2
+  && movingRect.y-fixedRect.y< movingRect.height/2+fixedRect.height/2&& fixedRect.y-movingRect.y< movingRect.height/2+fixedRect.height/2)
+  {
+  fixedRect.shapeColor="red";
+  movingRect.shapeColor="green";
+  text("colliding ", 500,600);
+}
+else{
+  fixedRect.shapeColor="yellow";
+  movingRect.shapeColor="yellow";
+}
+  drawSprites();
+}
